4f0b485325c6af40eda2d61e826354dd9b71aead
907howard <907howard@gmail.com>
2025-03-19 18:44:28 +0530

    14th march increment operator task

---
+++ src/Tasks_march/March14Task_Increment_decrement_operator.java
@@ -0,0 +1,10 @@
+package Tasks_march;
+
+public class March14Task_Increment_decrement_operator {
+    public static void main(String[] args) {
+        int a =10;
+        System.out.println(++a + a++ + a++); //(11 +11 +12)
+        System.out.println(a);
+
+    }
+}
---
+++ src/ex_07_Increment_Decrement_Op/lab069_In_de_op.java
@@ -0,0 +1,16 @@
+package ex_07_Increment_Decrement_Op;
+
+public class lab069_In_de_op {
+    public static void main(String[] args) {
+
+        int a =10;
+        int b = ++a;
+        System.out.println(b); //11
+        System.out.println(a); //11
+
+        // Exp and Result Table
+        //Line No /a /Result b
+        // 8 /10 / NA
+        //9 /
+    }
+}
